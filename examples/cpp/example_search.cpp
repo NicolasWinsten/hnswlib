e6e387942324eda61722f9116cdd1f29fb180f04
NicolasWinsten <nicolasd.winsten@gmail.com>
2023-03-28 11:44:27 -0700

    example file

--- examples/cpp/example_search.cpp
+++ examples/cpp/example_search.cpp
@@ -2,11 +2,11 @@
 
 
 int main() {
-    int dim = 16;               // Dimension of the elements
+    int dim = 128;               // Dimension of the elements
     int max_elements = 10000;   // Maximum number of elements, should be known beforehand
     int M = 16;                 // Tightly connected with internal dimensionality of the data
                                 // strongly affects the memory consumption
-    int ef_construction = 200;  // Controls index search speed/build speed tradeoff
+    int ef_construction = 300;  // Controls index search speed/build speed tradeoff
 
     // Initing index
     hnswlib::L2Space space(dim);
@@ -29,9 +29,22 @@ int main() {
     // Query the elements for themselves and measure recall
     float correct = 0;
     for (int i = 0; i < max_elements; i++) {
-        std::priority_queue<std::pair<float, hnswlib::labeltype>> result = alg_hnsw->searchKnn(data + i * dim, 1);
-        hnswlib::labeltype label = result.top().second;
-        if (label == i) correct++;
+        // compare deep search with standard hnsw search
+        // doesn't really indicate recall because deep search may give better results than hnsw search sometimes
+
+        std::priority_queue<std::pair<float, hnswlib::labeltype>> result = alg_hnsw->searchKnn(data + i * dim, 2);
+        hnswlib::labeltype label = result.top().second; // nearest neighbor of point i according to HNSW
+
+        auto myresult = alg_hnsw->deepSearch(i, 110);
+        // prune results to best two
+        // the first is the data point i itself
+        // the second is its approximate nearest neighbor
+        while (myresult.size() > 2) {
+            auto x = myresult.top();
+            myresult.pop();
+        }
+
+        if (label == myresult.top().second) correct++;
     }
     float recall = correct / max_elements;
     std::cout << "Recall: " << recall << "\n";
